8d54178b56715714cfe2b5c8092a78cf4dd3a4f9
ρяσƒєѕѕσя️️ <118790135+kumgit@users.noreply.github.com>
2022-12-15 12:42:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -204,7 +204,7 @@ async def next_page(bot, query):
             [
                 InlineKeyboardButton(
                     text=f"[{get_size(file.file_size)}] {file.file_name}",
-                    url=await get_shortlink(f"https://t.me/{temp.U_NAME}?start=files_{file.file_id}", callback_data=f'files#{nxreq}#{file.file_id}'
+                    url=await get_shortlink(f"https://t.me/{temp.U_NAME}?start=files_{file.file_id}", callback_data=f'files#{nxreq}#{file.file_id}',
                 ),
             ]
             for file in files
