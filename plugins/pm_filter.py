29e84129dd182bfb72c116a1a5066f01803a1c2a
ρяσƒєѕѕσя️️ <118790135+kumgit@users.noreply.github.com>
2022-12-20 09:49:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -173,7 +173,7 @@ async def pm_next_page(bot, query):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("okay...", show_alert=True)
     try:
         offset = int(offset)
     except:
@@ -255,7 +255,7 @@ async def next_page(bot, query):
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer("okDa", show_alert=True)
+        return await query.answer("okay...", show_alert=True)
     if movie_ == "close_spellcheck":
         return await query.message.delete()
     movies = SPELL_CHECK.get(query.message.reply_to_message.id)
@@ -924,7 +924,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('SOURCE CODE', url='https://github.com/kumgit/PROF-AF')
+            InlineKeyboardButton('SOURCE CODE', url='https://t.me/sstrangerrr')
             ],[
             InlineKeyboardButton('👩‍🦯 Back', callback_data='about')
         ]]
