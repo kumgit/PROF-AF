2531136c8c29e7f068a651e123529b7eeebd5140
ρяσƒєѕѕσя️️ <118790135+kumgit@users.noreply.github.com>
2023-02-24 11:42:52 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -3,7 +3,6 @@ from struct import pack
 import re
 import base64
 from pyrogram.file_id import FileId
-import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
 from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_RIST_BTNS
