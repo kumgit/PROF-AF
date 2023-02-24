60ccb7e84c654faed11e4deeeba4f185438ac49d
ρяσƒєѕѕσя️️ <118790135+kumgit@users.noreply.github.com>
2023-02-24 11:37:12 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -3,8 +3,8 @@ from struct import pack
 import re
 import base64
 from pyrogram.file_id import FileId
-from pymongo.errors import DuplicateKeyError
-from umongo import Instance, Document, fields
+import DuplicateKeyError
+import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
 from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_RIST_BTNS
