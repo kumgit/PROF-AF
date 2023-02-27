ab1e79526386f8db958a89aa3eafbe95e250cbea
ρяσƒєѕѕσя️️ <118790135+kumgit@users.noreply.github.com>
2023-02-27 18:33:49 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -10,20 +10,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils import temp
 import re
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)import logging
-import asyncio
-from pyrogram import Client, filters, enums
-from pyrogram.errors import FloodWait
-from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, ChatAdminRequired, UsernameInvalid, UsernameNotModified
-from info import ADMINS
-from info import INDEX_REQ_CHANNEL as LOG_CHANNEL
-from database.ia_filterdb import save_file
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from utils import temp
-import re
-logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-lock = asyncio.Lock()
 
 
 @Client.on_callback_query(filters.regex(r'^index'))
